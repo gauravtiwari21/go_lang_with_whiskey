3f4cef804ab109f49dcc198683378e52a23e4924
gauravtiwari21 <gauravtiwari21@gmail.com>
2016-12-29 12:16:12 +1100

    Moved helper tests as separate

--- main_test.go
+++ main_test.go
@@ -155,33 +155,3 @@ func TestStartsWithMissing_Requirement5(t *testing.T) {
 		t.Error("TEST FAILED: valid substring not found in main string: ", mainStr, findStr)
 	}
 }
-
-/* TODO delete or move following tests to a new separate module as helpers_test.go because 
-the follwing tests as they may not be required now as they are already validated by tests for calling functions
- 
-Test for a helper function used to remove duplicates in a list
-*/
-func TestRemoveDuplicates(t *testing.T){
-	inputLIst := []int {1,9,9,41}
-	outputSampleList := []int {1,9,41}
-	realOutput := []int (removeDuplicates(inputLIst))
-	for i := range outputSampleList {
-		if realOutput[i] != outputSampleList[i] {
-			t.Error("TEST FAILED: Expected output mismatch", realOutput)
-			break
-		}
-	}
-}
-
-// Test for a helper function used to count occurences of elements in a list
-func TestCountOccurences(t *testing.T){
-	inputLIst := []int {1,9,9,41}
-	outputSample := map[int]int {1:1,9:2,41:1}
-	realOutput := map[int]int (countOccurences(inputLIst))
-	for i := range outputSample {
-		if realOutput[i] != outputSample[i] {
-			t.Error("TEST FAILED: Expected output mismatch", realOutput, outputSample)
-			break
-		}
-	}
-}
